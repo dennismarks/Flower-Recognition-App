3e4bed09cc4bfabc7aff663ac4217da369a4f553
dennismarks <dennismarkshome@gmail.com>
2019-05-10 20:55:15 -0400

    flower classification using mlmodel

--- Flower Recognition App/ViewController.swift
+++ Flower Recognition App/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -22,14 +24,34 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
-        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[UIImagePickerController.InfoKey.editedImage]
-        imageView.image = image as? UIImage
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            guard let ciimage = CIImage(image: image) else {
+                fatalError("error converting to ciimage")
+            }
+            imageView.image = image
+            detect(image: ciimage)
+        }
         imagePicker.dismiss(animated: true, completion: nil)
     }
     
+    func detect(image: CIImage) {
+        // vncoremlmodel - comes from the vision framework; allows us to perform an image analysisn request that uses our coreml model to process images
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {fatalError("loading coreML model failed")}
+        let requst = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results?.first as? VNClassificationObservation else {fatalError("model failed to proccess image")}
+            self.navigationItem.title = result.identifier.capitalized
+        }
+        
+        // perfrom request
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([requst])
+        } catch {
+            print(error)
+        }
+    }
 }
 
